b8d3791c7618da39a440a56dbea246f5b1833b9f
onegunsamurai <soshrewdjudge@gmail.com>
2021-10-06 11:59:40 +0300

    TestCase Fail Test

---
+++ app/app/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+from app.calc import add
+
+
+class CalcTests(TestCase):
+
+    def test_add_numbers(self):
+        """Test if two numbers are added properly"""
+        self.assertEqual(add(1,3),3)
