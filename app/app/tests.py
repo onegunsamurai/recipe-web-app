76e74324beeabeecfeac83bd4fd32b144085c052
onegunsamurai <soshrewdjudge@gmail.com>
2021-10-06 12:01:06 +0300

    Test

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,4 +7,4 @@ class CalcTests(TestCase):
 
     def test_add_numbers(self):
         """Test if two numbers are added properly"""
-        self.assertEqual(add(1,3),3)
+        self.assertEqual(add(1,3),4)
