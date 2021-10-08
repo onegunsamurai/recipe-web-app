42db708511733dd25dab20e6d4c12715d92b3ae4
onegunsamurai <soshrewdjudge@gmail.com>
2021-10-08 16:59:51 +0300

    Edit

--- app/app/settings.py
+++ app/app/settings.py
@@ -82,13 +82,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
-    },
-    'test': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
+    }
 }
 
 
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -14,9 +14,11 @@ class CommandsTestCase(TestCase):
             gi.return_value = True
             call_command('wait_for_db')
             self.assertEqual(gi.call_count, 1)
-    @patch('time.sleep', value=None)
+
+    @patch('time.sleep', return_value=None)
     def test_wait_for_db(self, ts):
         """Test waiting for db"""
+
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
             gi.side_effect = [OperationalError] * 5 + [True]
             call_command('wait_for_db')
