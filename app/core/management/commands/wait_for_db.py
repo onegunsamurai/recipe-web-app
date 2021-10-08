321b4d0545bb8b0fda2b425f022fc43dc0a5c8a6
onegunsamurai <soshrewdjudge@gmail.com>
2021-10-08 17:05:48 +0300

    Edit

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -18,5 +18,8 @@ class Command(BaseCommand):
             except OperationalError:
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
+            except RuntimeWarning:
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
