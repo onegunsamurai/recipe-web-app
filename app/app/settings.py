d21b77befcf91ecd6fc9d0e99b11ba74608ddd03
onegunsamurai <soshrewdjudge@gmail.com>
2021-10-11 18:16:45 +0300

    Edit x2

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-j=i-edmftzx8hewzr#&=c_-uy+d6hf)-ubv=0p7wvbuz96s6sc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
